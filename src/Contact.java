871684042ab292957fecf2793234008f75fff948
SerguioL <serguio.l.landa@gmail.com>
2021-09-23 11:40:18 -0500

    first commit

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Contacts {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Contact {
+
+    //private properties
+    private String name;
+    private String phoneNumber;
+
+    //constructor
+
+
+    public Contact(String name,String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/ContactTest.java
+++ src/ContactTest.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class ContactTest {
+public class ContactTest {
+    public static void main(String[] args) {
+
+    }
 }
